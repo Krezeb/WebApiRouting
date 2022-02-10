93e5e1d5a044aff98a0416c1840a1b2a3d9e2b9b
Lachlan Morrison <lmorrison85@gmail.com>
2022-02-10 14:39:13 +0100

    Fetch and Display Expenses that belong to a specific UserId and Specific BudgetId - Groundwork complete

--- lachlan.js
+++ lachlan.js
@@ -1,4 +1,33 @@
-fetch('https://localhost:7073/ListAllUsers', { method: 'GET' })
+// fetch('https://localhost:7073/ListAllUsers', { method: 'GET' })
+//     .then(response => response.json()) //.json() is a function that converts the "response" from the fetch into a JSON object
+//     .then(function (data) {
+//         appendData(data);
+//     })
+
+// const appendData = (data) => {
+//     let body = document.getElementById('app')
+//     for (var i = 0; i < data.length; i++) {
+//         var div = document.createElement("div");
+//         div.innerHTML =
+//             'User Id : ' + data[i].userId + '<br/>' +
+//             'Name    : ' + data[i].name + '<br/>' +
+//             'E-Mail  : ' + data[i].email + '<br/>' +
+//             'Age     : ' + data[i].age + '<br/>' +
+//             'Password: ' + data[i].password + '<br/>' + '<br/>';
+//         body.appendChild(div);
+//     }
+// }
+
+// ListAllUsers();
+
+fetch
+    ('https://localhost:7073/GetExpenseForSpecificBudget',
+        {
+            method: 'POST',
+            headers: {'Content-type': 'application/json; charset=UTF-8'},
+            body: JSON.stringify({ userId: 1, budgetId: 1 })
+        },
+    )
     .then(response => response.json()) //.json() is a function that converts the "response" from the fetch into a JSON object
     .then(function (data) {
         appendData(data);
@@ -6,16 +35,48 @@ fetch('https://localhost:7073/ListAllUsers', { method: 'GET' })
 
 const appendData = (data) => {
     let body = document.getElementById('app')
-    for (var i = 0; i < data.length; i++) {
-        var div = document.createElement("div");
+
+    for (var i = 0; i < data.length; i++) 
+    {
+        let div = document.createElement("div");
+        
         div.innerHTML =
-            'User Id : ' + data[i].userId + '<br/>' +
-            'Name    : ' + data[i].name + '<br/>' +
-            'E-Mail  : ' + data[i].email + '<br/>' +
-            'Age     : ' + data[i].age + '<br/>' +
-            'Password: ' + data[i].password + '<br/>' + '<br/>';
-        body.appendChild(div);
+            'Budget Name : ' + data[i].budgetName + '<br/><br/>' + 'Expenses :' + '<br/>';
+            
+            body.appendChild(div);
+            
+            for (var j = 0; j < data[i].expenses.length; j++) 
+            {
+                let div2 = document.createElement("div");
+                div2.innerHTML =
+                    'Category Name : ' + data[i].expenses[j].categoryName + '<br/>' +
+                    'Amount : ' + data[i].expenses[j].amount + '<br/>' +
+                    'Recipient : ' + data[i].expenses[j].recipient + '<br/>' +
+                    'Date : ' + data[i].expenses[j].date + '<br/>' +
+                    'Comment : ' + data[i].expenses[j].comment + '<br/><br/>'; 
+                body.appendChild(div2);
+            }
     }
 }
 
-ListAllUsers();
+// const appendData = (data) => {
+//     let body = document.getElementById('app')
+
+//     for (var i = 0; i < data.length; i++) 
+//     {
+//         var div = document.createElement("div");
+//         div.innerHTML =
+//             'Budget Name : ' + data[i].budgetName + '<br/>'// +
+//             // 'Expenses : ' + data[i].expenses[i].amount;
+//             for (var j = 0; j < data.expenses.length; j++) 
+//             {
+//                 var div2 = document.createElement("div");
+//                 div.innerHTML =
+//                     'Category Name : ' + data[i].expenses[j].categoryName + '<br/>'// +
+//                     // 'Expenses : ' + data[i].expenses[i].amount;
+                    
+//                 body.appendChild(div2);
+//             }
+//         body.appendChild(div);
+//     }
+// }
